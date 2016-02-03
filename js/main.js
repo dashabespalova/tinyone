53f4c1072ead43f99cf291501a4ef57ad64480bf
dashabespalova <dasha_box1@mail.ru>
2016-02-03 10:07:41 +0200

    responsive menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-;(function(){
+// ;(function(){
+// 	var menuToggle = document.getElementById("ba-menu-toggle"),
+// 	body = document.body;
 
-})();
+// 	menuToggle.onclick = function (event){
+// 		event.preventDefault();
+// 		if(body.className === "ba-menu-opened"){
+// 			body.className ="";
+
+// 		} else {
+// 			body.className = "ba-menu-opened";
+// 		}
+
+// 	}
+
+// })();
+
+;(function($){
+	$(function(){
+		var body = $('body'),
+		menuToggle = $('.ba-menu-toggle');
+
+		menuToggle.click(function(event){
+			event.preventDefault();
+			body.toggleClass('ba-menu-opened');
+		});
+	});
+
+})(jQuery)
+
